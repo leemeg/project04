078dba91d3ec53f8f7bde9b22fd841ab1b961ca8
leemeg <marshalll@student.ncmich.edu>
2018-10-24 18:51:04 -0400

    final finished for good

--- project04.js
+++ project04.js
@@ -9,6 +9,11 @@
 const PROMPT = require('readline-sync');
 
 const STARTERTITLES = ["The Hate U Give", "Halloween", "First Man", "Venom", "Bad Times", "Night School", "Goosebumps", "Star is Born"];
+const MOVIE_TITLE = 0;
+const CURRENT_USER_RATING = 1;
+const ARRAY_OF_VOTES = 2;
+const NUM_VOTES = 3;
+const AVER_RATING = 4;
 
 let movieInfo = [];
 
@@ -30,31 +35,25 @@ main();
 
 
 function setmovieInfo() {
-    const MOVIE_TITLE = 0;
-    const CURRENT_USER_RATING = 1;
-    const ARRAY_OF_VOTES = 2;
-    const NUM_VOTES = 3;
-    const AVER_RATING = 4;
-
     let i = (movieInfo.length);
     if (i < 0){
         i = 0;
     }
     movieInfo[i] = [];
     console.log(`\x1Bc`);
-    while (! movieInfo[i][MOVIE_TITLE] || !/^[a-zA-Z -]{1,50}$/.test(movieInfo[i][0])){
+    while (! movieInfo[i][MOVIE_TITLE] || !/^[a-zA-Z -]{1,50}$/.test(movieInfo[i][MOVIE_TITLE])){
         movieInfo[i][MOVIE_TITLE] = PROMPT.question(`Please enter movie title: `);
         if (!/^[a-zA-Z -]{1,50}$/.test(movieInfo[i][MOVIE_TITLE])){
-            console.log(`${movieInfo[i][0]} is not valid, Please enter movie title)`);
+            console.log(`${movieInfo[i][MOVIE_TITLE]} is not valid, Please enter movie title)`);
         }
     }
     titleValue = i;
     setStars();
     movieInfo[i][CURRENT_USER_RATING] = Number(stars);
     movieInfo[i][ARRAY_OF_VOTES] = [];
-    movieInfo[i][ARRAY_OF_VOTES][0] = movieInfo[i][1];
+    movieInfo[i][ARRAY_OF_VOTES][0] = movieInfo[i][CURRENT_USER_RATING];
     movieInfo[i][NUM_VOTES] = 1;
-    movieInfo[i][AVER_RATING] = movieInfo[i][1];
+    movieInfo[i][AVER_RATING] = movieInfo[i][CURRENT_USER_RATING];
     console.log(`\x1Bc`);
     console.log(`\nThank you for your input. `)
 
@@ -102,9 +101,9 @@ function branchAction() {
             console.log(`Which movie would you like to rate? `);
             chooseTitle();
             setStars();
-            movieInfo[titleValue][1] = Number(stars);
-            movieInfo[titleValue][2].push(Number(stars));
-            movieInfo[titleValue][3]++;
+            movieInfo[titleValue][CURRENT_USER_RATING] = Number(stars);
+            movieInfo[titleValue][ARRAY_OF_VOTES].push(Number(stars));
+            movieInfo[titleValue][NUM_VOTES]++;
 
             break;
         case 2:
@@ -112,13 +111,13 @@ function branchAction() {
             console.log(`Which movie would you like to view? `);
             chooseTitle();
             calcAverage();
-            if (!/[0-9]/.test(movieInfo[titleValue][4])){
+            if (!/[0-9]/.test(movieInfo[titleValue][AVER_RATING])){
                 console.log(`\x1Bc`);
-                console.log(`${movieInfo[titleValue][0]} has not yet been rated. `)
+                console.log(`${movieInfo[titleValue][MOVIE_TITLE]} has not yet been rated. `)
             }
             else {
                 console.log(`\x1Bc`);
-                console.log(`\nThe average rating for ${movieInfo[titleValue][0]} is ${movieInfo[titleValue][4]} stars. `);
+                console.log(`\nThe average rating for ${movieInfo[titleValue][MOVIE_TITLE]} is ${movieInfo[titleValue][AVER_RATING]} stars. `);
             }
             break;
         case 3:
@@ -157,10 +156,10 @@ function setStars() {
 function calcAverage() {
     let sum = 0;
 
-    for (let i = 0; i < movieInfo[titleValue][2].length; i++) {
-        sum = sum + movieInfo[titleValue][2][i];
+    for (let i = 0; i < movieInfo[titleValue][ARRAY_OF_VOTES].length; i++) {
+        sum = sum + movieInfo[titleValue][ARRAY_OF_VOTES][i];
     }
-    movieInfo[titleValue][4] = (sum / movieInfo[titleValue][2].length).toFixed(1);
+    movieInfo[titleValue][AVER_RATING] = (sum / movieInfo[titleValue][ARRAY_OF_VOTES].length).toFixed(1);
 
 }
 
@@ -168,7 +167,7 @@ function setrateOrder() {
     let unsort = movieInfo.length - 1;
     for (let y = 0; y < unsort; y++) {
         for (let x = 0; x < unsort; x++){
-            if (movieInfo[x + 1][4] > movieInfo[x][4]){
+            if (movieInfo[x + 1][AVER_RATING] > movieInfo[x][AVER_RATING]){
                 let temp = movieInfo[x + 1];
                 movieInfo[x + 1] = movieInfo[x];
                 movieInfo[x] = temp;
